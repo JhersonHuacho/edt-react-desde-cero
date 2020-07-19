8d60c1479b75045c3ce3b8c7bffad3bab54f018f
Jherson <jhersonhuacho@gmail.com>
2020-07-19 11:48:53 -0500

    Clase 03: Componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,39 @@ import React from 'react';
 import "./styles/styles.scss";
 import Curso from './Curso';
 
+const cursos = [
+  {
+    titulo: "React desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/4c858734-9c72-45db-84ca-eb9e79cbe8df.png",
+    price: 10,
+    profesor: "Francisco Huacho"
+  },
+  {
+    titulo: "Angular desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/9e7655f4-a855-4d30-a3e0-736fa47f479b.png",
+    price: 20,
+    profesor: "Jordy Huacho"
+  },
+  {
+    titulo: "Vue desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/55f8eb30-f5b1-4495-a23d-938aede6a38d.png",
+    price: 30,
+    profesor: "Nayeli Huacho"
+  },
+  {
+    titulo: "Firebase desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/813e44ac-2d0b-4f1a-8629-f3da272a4c6a.png",
+    price: 40,
+    profesor: "Dayana Huacho"
+  },
+  {
+    titulo: "Node desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/671fe4c0-bbac-4bbe-8a0e-3fb0811a8353.png",
+    price: 50,
+    profesor: "Daneri Huacho"
+  }
+];
+
 const App = () => (
   <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -18,10 +51,9 @@ const App = () => (
     </div>
 
     <div className="ed-grid m-grid-3">
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
+      {
+        cursos.map(curso => <Curso title={curso.titulo} image={curso.image} price={curso.price} profesor={curso.profesor} />)
+      }
     </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-const Curso = () => (
+import PropTypes from 'prop-types';
+
+//const Curso = props => (
+const Curso = ({ title, image, price, profesor }) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://edteam-media.s3.amazonaws.com/courses/original/4c858734-9c72-45db-84ca-eb9e79cbe8df.png" alt="Poster del curso" />
+            <img src={image} alt={title} />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 className="t5 s-mb-2 s-center">
-                Programación orientada a objetos con Go
-          </h3>
+                {title}
+            </h3>
             <div className="s-mb-2 s-main-center">
                 <div className="card__teacher s-cross-center">
                     <div className="card__avatar s-mr-1">
@@ -15,14 +18,28 @@ const Curso = () => (
                             <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/4c86750c-af6a-4423-9748-54ad70f33a90.jpg" alt="foto" />
                         </div>
                     </div>
-                    <span className="small">Alexys Lozada</span>
+                    <span className="small">{profesor}</span>
                 </div>
             </div>
             <div className="s-main-center">
-                <a className="button--ghost-alert button--tiny" href="http//ed.team">$ 20USD</a>
+                <a className="button--ghost-alert button--tiny" href="http//ed.team">{`$ ${price}`}</a>
             </div>
         </div>
     </article>
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.string,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+    title: "No se encontró título",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/4feb100b-5d47-4daf-b421-9983c5ecdfa8.png",
+    price: "--",
+    profesor: ""
+}
 
 export default Curso;
